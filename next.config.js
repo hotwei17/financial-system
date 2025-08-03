f4261fdf8223388e662bcba9960a39eb26a2a6fd
hotwei17 <hotwei17@life.fubon.com>
2025-08-03 14:36:53 +0800

    Upload simplified JavaScript version without TypeScript
    Remove TypeScript dependencies and simplify code structure

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    serverComponentsExternalPackages: ['@planetscale/database']
-  }
 }
 
 module.exports = nextConfig
